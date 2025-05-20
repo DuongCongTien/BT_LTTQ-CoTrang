4f45e9faaf0a4dc4f8c14ff0fbce65369c1c742f
DƯƠNG CÔNG TIẾN <dctcuncon@gmail.com>
2025-05-20 23:27:06 +0700

    Khôi phục thư mục giao diện từ nhánh Hieu

--- BT_NHOMCOTRANGG/BT_NHOMCOTRANGG/Form1.vb
+++ BT_NHOMCOTRANGG/BT_NHOMCOTRANGG/Form1.vb
@@ -1,115 +1,38 @@
 ﻿Public Class Form1
-
-
-    Private Sub ThêmVéMáyBayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ThêmVéMáyBayToolStripMenuItem.Click
-        Dim f As New veMayBay
-        f.Show()
+    Private Sub btnKhachHang_Click(sender As Object, e As EventArgs)
+        Dim KhachHang As New khachHang()
+        KhachHang.Show()
     End Sub
 
-    Private Sub ThêmKháchHàngToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ThêmKháchHàngToolStripMenuItem.Click
-        Dim f As New khachHang
-        f.Show()
+    Private Sub btnHoaDon_Click(sender As Object, e As EventArgs)
+        Dim HoaDon As New hoaDon()
+        HoaDon.Show()
     End Sub
 
-    Private Sub SânBayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SânBayToolStripMenuItem.Click
-        Dim f As New sanBay
-        f.Show()
+    Private Sub btnHangHangKhong_Click(sender As Object, e As EventArgs)
+        Dim HangHangKhong As New hangHangKhong()
+        HangHangKhong.Show()
     End Sub
 
-    Private Sub ChuyếnBayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChuyếnBayToolStripMenuItem.Click
-        Dim f As New ChuyenBay
-        f.Show()
+    Private Sub btnChuyenBay_Click(sender As Object, e As EventArgs)
+        Dim ChuyenBay As New ChuyenBay()
+        ChuyenBay.Show()
     End Sub
 
-    Private Sub HãngHàngKhôngToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HãngHàngKhôngToolStripMenuItem.Click
-        Dim f As New hangHangKhong
-        f.Show()
+    Private Sub btnVeMayBay_Click(sender As Object, e As EventArgs)
+        Dim VeMayBay As New veMayBay()
+        VeMayBay.Show()
     End Sub
 
-    Private Sub VéKhứHồiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VéKhứHồiToolStripMenuItem.Click
-        Dim f As New veKhuHoi
-        f.Show()
+    Private Sub btnVeKhuHoi_Click(sender As Object, e As EventArgs)
+        Dim VeKhuHoi As New veKhuHoi()
+        VeKhuHoi.Show()
     End Sub
 
-    Private Sub HóaĐơnToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HóaĐơnToolStripMenuItem.Click
-        Dim f As New hoaDon
-        f.Show()
+    Private Sub btnSanBay_Click(sender As Object, e As EventArgs)
+        Dim SanBay As New sanBay()
+        SanBay.Show()
     End Sub
 
 
-    'Tim kiem
-    Private Sub VéMáyBayToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles VéMáyBayToolStripMenuItem1.Click
-        Dim f As New timKiemVe
-        f.Show()
-    End Sub
-
-    Private Sub KháchHàngToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KháchHàngToolStripMenuItem.Click
-        Dim f As New timKiemKhachHang
-        f.Show()
-    End Sub
-
-    Private Sub SânBayToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles SânBayToolStripMenuItem2.Click
-        Dim f As New timKiemSanBay
-        f.Show()
-    End Sub
-
-    Private Sub ChuyếnBayToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ChuyếnBayToolStripMenuItem2.Click
-        Dim f As New timKiemChuyenBay
-        f.Show()
-    End Sub
-
-    Private Sub HãngHàngKhôngToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles HãngHàngKhôngToolStripMenuItem2.Click
-        Dim f As New timKiemHHK
-        f.Show()
-    End Sub
-
-    Private Sub VéKhứHồiToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles VéKhứHồiToolStripMenuItem2.Click
-        Dim f As New timKiemVeKhuHoi
-        f.Show()
-    End Sub
-
-    Private Sub HóaĐơnToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles HóaĐơnToolStripMenuItem2.Click
-        Dim f As New timKiemHD
-        f.Show()
-    End Sub
-
-    'Thong ke
-
-    Private Sub VéMáyBayToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles VéMáyBayToolStripMenuItem2.Click
-        Dim f As New reportVeMayBay
-        f.Show()
-    End Sub
-
-    Private Sub KháchHàngToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles KháchHàngToolStripMenuItem2.Click
-        Dim f As New reportKH
-        f.Show()
-    End Sub
-
-    Private Sub SânBayToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles SânBayToolStripMenuItem3.Click
-        Dim f As New reportSanBay
-        f.Show()
-    End Sub
-
-    Private Sub ChuyếnBayToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ChuyếnBayToolStripMenuItem3.Click
-        Dim f As New reportChuyenBay
-        f.Show()
-    End Sub
-
-    Private Sub HóaĐơnToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles HóaĐơnToolStripMenuItem3.Click
-
-    End Sub
-
-    'Tinh toan
-
-    Private Sub TổngDoanhThuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TổngDoanhThuToolStripMenuItem.Click
-
-    End Sub
-
-    Private Sub SốTiềnKHPhảiTrảToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SốTiềnKHPhảiTrảToolStripMenuItem.Click
-
-    End Sub
-
-    Private Sub SốLượngChuyếnToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SốLượngChuyếnToolStripMenuItem.Click
-
-    End Sub
 End Class
